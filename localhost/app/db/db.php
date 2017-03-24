37437278b474d3ed4bbc19601c59df59bf749f7d
davidmartinez99 <davidmartinez8925@gmail.com>
2017-03-24 12:59:46 -0700

    newpage

---
+++ localhost/app/db/db.php
@@ -0,0 +1,28 @@
+
+<?php
+class Database
+{   
+    private $host = "localhost";
+    private $db_name = "app";
+    private $username = "dmd";
+    private $password = "david8925";
+    public $conn;
+     
+    public function dbConnection()
+	{
+     
+	    $this->conn = null;    
+        try
+		{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        }
+		catch(PDOException $exception)
+		{
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
+?>
