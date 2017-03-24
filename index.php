209debaa09a5c47d5626999311ba9c63ff5a9e9d
davidmartinez99 <davidmartinez8925@gmail.com>
2017-03-24 13:40:38 -0700

    newpage

--- index.php
+++ index.php
@@ -1,6 +1,56 @@
-welcome to drupal
-<?php
-phpinfo(); 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="icon" href="img/flags/golf.png" type="image/png" sizes="16x16">
+	<title>Golf.ioe</title>
+	<!--No Href external links to css or js -->
+	<link rel="stylesheet" href= "css/custom.css">
+		<style>
+		#if-location { 
+		position: absolute; left: 10px; top:200px;
+		width:800px
+		height:500px
+	}
+</style>
+
+</head>
+<body>
+<!-- Dropdown -->
+<div class="dropdown">
+  <button class="dropbtn" ><img src = "img/star.png" style="width:40px;height:40px;"></button>  <!- size is w-40 h-40 -!>
+  <div class="dropdown-content">
+	<a href="phpcode/" >Phpcode</a>
+	<a href="downloads/" >Downloads</a>
+	<a href="emp/" >PMA</a>
+	<a href="logs/" target = " blank" >Logs</a>
+	<a href="html/" target = " blank" >HTML</a>
+	<a href="bootstrap/" target = " blank" >niceweb</a>
+	<a href="https://sites.google.com/a/tritechsc.org/davidm" target = " blank" >Google Site</a>
+	<a href="https://sites.google.com/tritechsc.org/davidmd" target = " blank" >New Google Site</a>
+	
+  </div>
+</div>
+<!-- Links --!>
+
+<a href="phpcode/" class = "slink">Phpcode</a>&nbsp;
+<a href="downloads/" class = "slink">Downloads</a>&nbsp;
+<a href="emp/" class = "slink">PMA</a>&nbsp;
+<a href="logs/" target = " blank" class = "slink">Logs</a>&nbsp;
+<a href="html/" target = " blank" class = "slink">HTML</a>&nbsp;
+<a href="niceweb/" target = " blank" class = "slink">niceweb</a>&nbsp;
+<a href="http://10.183.2.7:8080" target = "_blank" class = "slink">NodeServer 8080</a>&nbsp;
+<a href="http://10.183.2.7:3000" target = "_blank" class = "slink">NodeServer 3000</a>&nbsp;
+<a href="http://10.183.2.7/php" target = "_blank" class = "slink">Php</a>&nbsp;
+<a href="https://sites.google.com/a/tritechsc.org/davidm" target = " blank" class = "slink">Google Site</a>
+<a href="https://sites.google.com/tritechsc.org/davidmd" target = " blank" class = "slink">New Google Site</a>
 
 
-?>
+
+
+
+
+
+<!-- <iframe id="if-location" name="if" scr="intro.html" scrolling="yes" frameborder="3"></iframe> -->
+
+</body>
+</html>
